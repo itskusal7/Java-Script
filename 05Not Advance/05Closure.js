6f9928f3449a6f2835f0c6b6a3157267e8340066
Kushal <kushal@gmail.com>
2025-04-29 12:26:04 +0530

    add till binding

--- 05Not Advance/05Closure.js
+++ 05Not Advance/05Closure.js
@@ -1,13 +1,24 @@
-function init() {
-    var firstName = "Kushal";
-    console.log("I am init");
+// function init() {
+//     var firstName = "Kushal";
+//     console.log("I am init");
     
-    function sayFirstName(){
-        console.log(firstName);
-    }
-    return sayFirstName;
+//     function sayFirstName(){
+//         console.log(firstName);
+//     }
+//     return sayFirstName;
+// }
+
+// var value = init();
+
+// value();
+
+function doAddition(x) {
+    return function (y) {
+        return x+y;
+    };
 }
+    
+var add5 = doAddition(4);
+console.log(add5(5));
 
-var value = init();
-
-value();
+// clouser are the bundel of functions in a function within a enclouse environment.
---
+++ 05Not Advance/06thisBinding.js
@@ -0,0 +1,30 @@
+const kushal = {
+    firstName : "Kushal",
+    lastName : "Sapkota",
+    role : "Admin",
+    courseCount : 3,
+    getInfo : function(){
+        console.log(`
+            First name is ${this.firstName}
+            Last name is ${this.lastName}
+            His role is  ${this.role}
+            and he is studying ${this.courseCount} courses  
+        `);
+        
+    }
+}
+
+const dj = {
+    firstName : "Rock",
+    lastName : "DJ",
+    role : "Sub-Admin",
+    courseCount : 4,
+};
+
+// kushal.getInfo();
+// dj.getInfo();
+
+var djInfo = kushal.getInfo.bind(dj);
+djInfo();
+
+// bind always give you a reference back and you have to run the reference.
